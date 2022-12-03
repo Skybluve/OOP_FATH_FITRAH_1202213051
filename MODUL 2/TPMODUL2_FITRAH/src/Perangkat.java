4b114025a65a54efe9a7580510c20cbc5b6af6f4
Skybluve <lagaplays@gmail.com>
2022-12-03 09:51:52 +0700

    first commit

---
+++ MODUL 2/TPMODUL2_FITRAH/src/Perangkat.java
@@ -0,0 +1,38 @@
+
+
+public class Perangkat{
+    protected String drive;
+    protected int ram;
+    protected float processor;
+
+    public Perangkat( String drive, int ram, float processor){
+        this.drive = drive;
+        this.ram = ram;
+        this.processor = processor;
+    }
+    public String getDrive(){
+        return drive;
+    }
+    public void setDrive(String drive){
+        this.drive = drive;
+    }
+    public int getRam(){
+        return ram;
+    }
+    public void setRam(int ram){
+        this.ram = ram;
+    }
+    public float getProcessor(){
+        return processor;
+    }
+    public void setProcessor(float processor){
+        this.processor = processor;
+    }
+    public void informasi(){
+        System.out.println("");
+    
+    }
+
+    
+
+}
